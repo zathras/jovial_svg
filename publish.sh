4ffb2d70fc5645401293de53c4dd79a38133ded6
Bill Foote <billf@jovial.com>
2021-10-30 16:28:39 -0700

    Add changelist to publish checklist.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,9 @@
 # Run dartfmt on everything of interest, without distraction from
 # generated code.
 ./dartfmt_all.sh
+echo ""
+echo "***  Has CHANGELOG.md been updated?  ***"
+echo ""
+printf "          (Press return.)  "
+read x
 flutter pub publish
