ffd344e61926b51531601315f502e715e52f9e13
Bill Foote <billf@jovial.com>
2026-09-15 20:38:54 +0200

    Misspelled asset file name

--- test/test_main.dart
+++ test/test_main.dart
@@ -120,11 +120,12 @@ Future<void> _testSvgSiSame(Directory svgDir, Directory? outputDir) async {
         _checkDrawingSame(fromSvg, fromSvgC, '$ent differs');
         if (ent.path != 'demo/assets/svg/anglo.svg' &&
             ent.path != 'demo/assets/svg/anglo_bitmap.svg' &&
-            ent.path != 'demo/assets/svg/swiss-xvii.svg.svg') {
+            ent.path != 'demo/assets/svg/swiss-xvii.svg') {
           // @@ explain this
           expect(siB, svgcB);
         }
       } catch (failure) {
+        print('Failed on file ${ent.path}');
         fail(svgB, svgcB, true);
         rethrow;
       }
